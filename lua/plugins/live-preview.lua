b5594b83c81b3efac60524116f17427e7da1357d
Neilos-Thumm <parun.bcc37884@gmail.com>
2026-05-09 18:40:18 +1000

    add live preview plugin

---
+++ lua/plugins/live-preview.lua
@@ -0,0 +1,12 @@
+return {
+  "brianhuster/live-preview.nvim",
+  cmd = "LivePreview",
+  config = function()
+    vim.api.nvim_create_autocmd({ "InsertLeavePre", "TextChanged", "TextChangedP" }, {
+      pattern = "*.html,*.css,*.js",
+      callback = function()
+        vim.cmd("silent! write")
+      end,
+    })
+  end,
+}
