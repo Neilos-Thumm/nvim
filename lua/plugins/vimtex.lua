d9890c05ac719ddb330475a70c8b129f84e2e4e6
Neilos-Thumm <parun.bcc37884@gmail.com>
2026-06-22 14:19:27 +1000

    adjust concealment Vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -7,6 +7,21 @@ return {
     vim.g.vimtex_quickfix_mode = 0 -- don't auto-pop the error list
     vim.g.vimtex_view_skim_sync = 1 -- sync Skim's position after compile
     vim.g.vimtex_view_skim_activate = 1 -- bring Skim to focus after compile
+    vim.g.vimtex_syntax_conceal = {
+      accents = 1,
+      ligatures = 1,
+      cites = 1,
+      fancy = 1,
+      spacing = 0, -- don't conceal \vspace, \hspace, etc.
+      greek = 1, -- keep α, β, etc.
+      math_bounds = 1,
+      math_delimiters = 1,
+      math_fracs = 1,
+      math_super_sub = 1,
+      math_symbols = 1, -- keep ∑, ∫, etc.
+      sections = 0,
+      styles = 1,
+    }
     -- optional: match SeniorMars's engine choice
     -- vim.g.vimtex_compiler_latexmk_engines = { ["_"] = "-lualatex -shell-escape" }
   end,
